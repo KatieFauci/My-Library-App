c77dd401fd5decb15c7879f1b0cecece0b0a20de
Katie <66881113+KatieFauci@users.noreply.github.com>
2024-10-09 13:47:26 -0400

    Created virtual enviorment and set up initial file structure for eel application

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # My-Library-App
+
+# Start Virtual Enviorment (Windows)
+```
+venv\Scripts\activate
+```
---
+++ app.py
@@ -0,0 +1,7 @@
+import eel
+
+# Set web files folder
+eel.init('GUI')
+
+# Start
+eel.start('index.html', size=(1500, 800)) 
